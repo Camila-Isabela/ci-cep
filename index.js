f66e42e050cfd93bc9d70278ebbb622d846905a0
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2021-06-09 18:55:35 -0300

    ...

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function formatCep(value) {
+    return String(value)
+    .replace(/\D/g, "")
+    .slice(0, 8)
+    .replace(/(\d{5})(\d)/, "$1-$2")
+
+}
+
+module.exports = {
+    formatCep
+}
